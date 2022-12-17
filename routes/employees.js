8305704efcd8ed0ef8ecaeea39280e61571265c8
BorisBil <95983063+BorisBil@users.noreply.github.com>
2022-12-17 12:53:49 -0500

    Add Employee Route Added

--- routes/employees.js
+++ routes/employees.js
@@ -17,13 +17,17 @@ router.get('/:id', ash(async(req, res) => {
 
 /** EDIT EMPLOYEE */
 router.put('/:id', ash(async(req, res) => {
-    await Employee.update(req.body, {
-    where: {
-        id: req.params.id
-    }
-    });
+    await Employee.update(req.body, 
+        { where: { id: req.params.id } }
+    );
     let employee = await Employee.findByPk(req.params.id, {include: [Task]});
     res.status(201).json(employee);
+}));
+
+/** ADD NEW EMPLOYEE */
+router.post('/', ash(async(req, res) => {
+    let newEmployee = await Employee.create(req.body);
+    res.status(200).json(newEmployee);
 }));
 
 module.exports = router;
